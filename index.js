164b5afde45da087f798c749a869a19764e517c6
John <jackbull328@gmail.com>
2017-01-21 07:48:21 -0800

    Add email

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 var express = require("express"),
-    stormpath = require("express-stormpath"),
-    bodyParser = require("body-parser"),
-    MongoClient = require('mongodb').MongoClient,
-    mongoose = require("mongoose");
-    app = express();
-    require('dotenv').config();
-    nodemailer = require('nodemailer');
-    const transportString = `smtps://${process.env.MAIL_ADDRESS}%40gmail.com:${process.env.MAIL_PASS}@smtp.gmail.com`;
-    const transporter = nodemailer.createTransport(transportString);
-    console.log(transportString);
+  stormpath = require("express-stormpath"),
+  bodyParser = require("body-parser"),
+  MongoClient = require('mongodb').MongoClient,
+  mongoose = require("mongoose");
+app = express();
+require('dotenv').config();
+nodemailer = require('nodemailer');
+const transportString = `smtps://${process.env.MAIL_ADDRESS}%40gmail.com:${process.env.MAIL_PASS}@smtp.gmail.com`;
+const transporter = nodemailer.createTransport(transportString);
+console.log(transportString);
 
-    mongoose.Promise = require('bluebird');
+mongoose.Promise = require('bluebird');
 
-    let URL = "mongodb://heroku_j1647s3l:tkmie8pbencj50ljm1d688h87e@ds117919.mlab.com:17919/heroku_j1647s3l";
+let URL = "mongodb://heroku_j1647s3l:tkmie8pbencj50ljm1d688h87e@ds117919.mlab.com:17919/heroku_j1647s3l";
 
 
 
-    MongoClient.connect(URL, function(err, db) {
-     if (err) {
-       URL = 'mongodb://localhost:27017/mydatabase';
-     } else {
-       URL = process.env.URL;
-     }
-     db.close();
-    });
-    mongoose.connect(URL);
+MongoClient.connect(URL, function(err, db) {
+  if (err) {
+    URL = 'mongodb://localhost:27017/mydatabase';
+  } else {
+    URL = process.env.URL;
+  }
+  db.close();
+});
+mongoose.connect(URL);
 
-console.log("HERE I AM!!!",process.env.STORMPATH_API_KEY_ID)
+console.log("HERE I AM!!!", process.env.STORMPATH_API_KEY_ID)
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded());
@@ -43,31 +43,34 @@ app.use(stormpath.init(app, {
 }));
 
 var organizationSchema = new mongoose.Schema({
-  orgName : String,
-  givenName : String,
-  surname : String,
+  orgName: String,
+  givenName: String,
+  surname: String,
 });
 
 var Organization = mongoose.model("Organization", organizationSchema);
 
 var customerSchema = new mongoose.Schema({
-  custFirstName : String,
-  custLastName : String,
-  custAddress : String,
-  cusEmail : String,
+  custFirstName: String,
+  custLastName: String,
+  custAddress: String,
+  cusEmail: String,
   organization: String,
 });
 
-const organizationalDataAlreadyGiven =(req,res,next) =>{
+const organizationalDataAlreadyGiven = (req, res, next) => {
 
-  Organization.findOne({givenName:req.user.givenName,surname:req.user.surname}, function(err, Organization) {
+  Organization.findOne({
+    givenName: req.user.givenName,
+    surname: req.user.surname
+  }, function(err, Organization) {
     console.log(Organization);
     if (Organization) {
-        res.redirect('/customer'+Organization.orgName)
+      res.redirect('/customer' + Organization.orgName)
     } else {
       next();
     }
-})
+  })
 };
 
 
@@ -79,19 +82,26 @@ app.get("/", function(req, res) {
   res.render("landing");
 });
 
-app.post("/getCustomerInfo", stormpath.loginRequired, function(req, res){
-  Organization.findOne({givenName:req.user.givenName,surname:req.user.surname}, function(err, Organization) {
-    Customer.find({organization:Organization.orgName},function(err,people){
-        res.send(people);
+app.post("/getCustomerInfo", stormpath.loginRequired, function(req, res) {
+  Organization.findOne({
+    givenName: req.user.givenName,
+    surname: req.user.surname
+  }, function(err, Organization) {
+    Customer.find({
+      organization: Organization.orgName
+    }, function(err, people) {
+      res.send(people);
     })
   })
 });
 
 app.get("/customer:id", stormpath.loginRequired, function(req, res) {
-    res.render("customer", {organization: req.params.orgName});
+  res.render("customer", {
+    organization: req.params.orgName
+  });
 });
 
-app.get("/newOrganization",stormpath.loginRequired, function(req, res) {
+app.get("/newOrganization", stormpath.loginRequired, function(req, res) {
   res.render("newOrganization");
 });
 
@@ -100,7 +110,11 @@ app.post("/newOrganization", stormpath.loginRequired, organizationalDataAlreadyG
   var orgName = req.body.orgName;
   var givenName = req.body.givenName;
   var surname = req.body.surname;
-  var newOrganization = {orgName: orgName, givenName: givenName, surname: surname};
+  var newOrganization = {
+    orgName: orgName,
+    givenName: givenName,
+    surname: surname
+  };
 
   Organization.create(newOrganization, function(err, newlyCreated) {
     if (err) {
@@ -111,9 +125,9 @@ app.post("/newOrganization", stormpath.loginRequired, organizationalDataAlreadyG
   });
 });
 
-app.post('/logout', (req,res)=>{
+app.post('/logout', (req, res) => {
   console.log('logigin out')
-    res.redirect('/');
+  res.redirect('/');
 })
 
 app.post("/newCustomer", stormpath.loginRequired, function(req, res) {
@@ -122,9 +136,18 @@ app.post("/newCustomer", stormpath.loginRequired, function(req, res) {
   var custLastName = req.body.cusLastName;
   var custAddress = req.body.cusAddress;
   var cusEmail = req.body.cusEmail;
-  Organization.findOne({givenName:req.user.givenName,surname:req.user.surname}, function(err, organization) {
+  Organization.findOne({
+    givenName: req.user.givenName,
+    surname: req.user.surname
+  }, function(err, organization) {
     console.log("here is ORG!", organization);
-    var newCustomer = {custFirstName, custLastName, custAddress, cusEmail, organization:organization.orgName};
+    var newCustomer = {
+      custFirstName,
+      custLastName,
+      custAddress,
+      cusEmail,
+      organization: organization.orgName
+    };
     if (err) {
       console.log(err);
     } else {
@@ -136,14 +159,19 @@ app.post("/newCustomer", stormpath.loginRequired, function(req, res) {
         }
       });
 
-  };
+    };
 
   })
 })
 
 app.get("/newEmail", stormpath.loginRequired, function(req, res) {
-    Organization.findOne({givenName:req.user.givenName,surname:req.user.surname}, function(err, organization) {
-    res.render(("newEmail"), {organization,})
+  Organization.findOne({
+    givenName: req.user.givenName,
+    surname: req.user.surname
+  }, function(err, organization) {
+    res.render(("newEmail"), {
+      organization,
+    })
   })
 });
 
@@ -151,43 +179,40 @@ app.post("/newEmail", stormpath.loginRequired, function(req, res) {
 
   const message = req.body.message;
   const header = req.body.header;
-  const people= req.body.people;
-  console.log(message,header,people);
+  const people = req.body.people;
+  console.log(message, header, people);
 
-for (let i =0; i<people.length;i++){
+  for (let i = 0; i < people.length; i++) {
 
 
-  var mailOptions = {
-   from: '"Krishan Arya :busts_in_silhouette:" <dummyacct101390@gmail.com>', // sender address
-   to: people[i][3], // list of receivers
-   subject: header, // Subject line
-   text: `Dear ${people[i][0]}\n` + message, // plaintext body
-   html: `Dear ${people[i][0]}<br>,`+ message+"<a href='https://mighty-mountain-31348.herokuapp.com/redirect/"+people[i][3]+"'"+">Interested</a>"
-, // html body
-};
+    var mailOptions = {
+      from: '"Krishan Arya :busts_in_silhouette:" <dummyacct101390@gmail.com>', // sender address
+      to: people[i][3], // list of receivers
+      subject: header, // Subject line
+      text: `Dear ${people[i][0]}\n` + message, // plaintext body
+      html: `Dear ${people[i][0]},<br>` + message + "<br><a href='https://mighty-mountain-31348.herokuapp.com/redirect/" + people[i][3] + "'" + ">Interested</a>", // html body
+    };
 
-transporter.sendMail(mailOptions, function(error, info){
-   if(error){
-       return console.log(error);
-   }
-   console.log('Message sent: ' + info.response);
+    transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+        return console.log(error);
+      }
+      console.log('Message sent: ' + info.response);
+    });
+
+  }
+
 });
 
-}
 
-});
+app.get('/redirect/*', function(req, res) {
+      console.log("visited2");
+      var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+      console.log("this should be email", fullUrl.slice(fullUrl.lastIndexOf('/')); res.redirect('https://www.google.com/')
+    });
 
-
-app.get('/redirect/*',function(req,res){
-  console.log("visited2");
-   var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-   console.log(fullUrl);
-res.redirect('https://www.google.com/')
-})
-
-app.get('*', (req,res)=>{
-    res.redirect('/');
-})
-app.listen(process.env.PORT || 3000, function() {
-  console.log("The CRM Server is running");
-});
+    app.get('*', (req, res) => {
+      res.redirect('/');
+    }) app.listen(process.env.PORT || 3000, function() {
+      console.log("The CRM Server is running");
+    });
